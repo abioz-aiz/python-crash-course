0965035b26c30cf685e6cc58069ccd5a675c1bd3
Zoiba Zia <zoiba.zia27@gmail.com>
2021-11-08 20:57:28 +0530

    Chapter 2

---
+++ name.py
@@ -0,0 +1,16 @@
+name="ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#full_name.py
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+ 
+ #apostrophe.py
+
+message='One of Python\'s code\'s is malfunctioning'
+print(message)
